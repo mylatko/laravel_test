34fb5e47faa210d92bd71e448b9a532b5f48482d
nadya <pinnadya@gmail.com>
2020-09-11 15:55:11 +0700

    add migrations

---
+++ database/migrations/2020_09_11_071343_create_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('count');
+            $table->timestamp('appearance_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
---
+++ database/migrations/2020_09_11_071352_create_rent_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rent', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('book_id');
+            $table->integer('count');
+            $table->timestamp('begin_date');
+            $table->timestamp('finish_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rent');
+    }
+}
