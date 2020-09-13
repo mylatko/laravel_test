390a5e8fc22444f44f7b23257b31df8cc58bffa5
nadya <pinnadya@gmail.com>
2020-09-13 23:47:38 +0700

    add redis, index for rent table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Book;
 use App\Models\Rent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 
 class HomeController extends Controller
 {
@@ -26,7 +27,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $books = \App\Models\Book::paginate(5);
+        $currentPage = \request()->get('page', 1);
+
+//        Redis::set('books-'.$currentPage, serialize(\App\Models\Book::paginate(5)));
+        $books = unserialize(Redis::get('books-'.$currentPage));
+        if (!$books) {
+            Redis::set('books-'.$currentPage, json_encode(\App\Models\Book::paginate(5)));
+        }
 
         $user = Auth::user();
         $rentList = [];
